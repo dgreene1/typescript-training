806cfc82c5778fe63f7c537c8b6b3868614f377e
Dan Greene <dgreene@frontlinetechnologies.com>
2018-11-09 14:47:29 -0500

    improvements to documentation. Also bumped to the newest typescript

--- lessons/8-better-contracts-via-aggregate-types.ts
+++ lessons/8-better-contracts-via-aggregate-types.ts
@@ -1,19 +1,22 @@
 (()=>{  // <-- for this demo, we are using an Immediately Invoked Function Expression (IIFE) so that the types and functions don't bleed into the other lessons
 
+    const mockBearerTokenWeExpect = "785a31de-2df8-455b-b661-57618e9b9f32";
+
     interface IPaidTimeOff {
         reason: string,
         employeeId: string,
         startDate: Date,
         endDate: Date
-        employeeIdOfVerifier: string | undefined,
-        verifiedDate: Date | undefined,
+        employeeIdOfVerifier?: string | undefined,
+        verifiedDate?: Date | undefined,
     }
 
     // Step #1: Can you find what's wrong with this function?
-    //      Hint: if a consumer of this function is expecting to get back every property, they will get strange results
-    function PostAnVerifiedAbsence(pto: IPaidTimeOff, bearerToken: string, verify: boolean) : IPaidTimeOff {
+    //      See below for a hint. Or you can just run this lesson to see.
+    // Step #2: Can you use the aggregate type operator (&) to make the output more clear? (see the "lessons_answers" folder for more)
+    function PostAnAbsence(pto: IPaidTimeOff, bearerToken: string, verify: boolean) : IPaidTimeOff {
         var allowedToVerify = CanVerify(bearerToken);
-        if(allowedToVerify){
+        if(!allowedToVerify){
             throw new Error("This user is not authorized to verify");
         }
         if(verify){
@@ -26,11 +29,21 @@
         return pto;
     }
 
+    // Hint
+    const verifyThisPlease = true;
+    var verifiedPto = PostAnAbsence({
+        reason: "I was sick",
+        employeeId: "1234567",
+        startDate: new Date(),
+        endDate: new Date("9999-12-31T00:00.000Z")
+    }, mockBearerTokenWeExpect, verifyThisPlease)
+    console.log("This PTO was verified by " + verifiedPto.employeeIdOfVerifier + " on " + verifiedPto.verifiedDate); // <-- Hint Hint
+
     function CanVerify(bearerToken: string) : boolean {
         // In the real world, this would be hitting an external service to look up the "claims" to check
         //      does this user have a claim that states that they can verify?
         //      if so, let's return who they are and stamp the time they opted to verify
-        if(bearerToken === "785a31de-2df8-455b-b661-57618e9b9f32"){
+        if(bearerToken === mockBearerTokenWeExpect){
             return true;
         } else {
             return false;
@@ -38,8 +51,8 @@
     }
 
     function GetEmployeeName(bearerToken: string): string {
-        // Pretend that it gets the name from the DB
-        if(bearerToken === "785a31de-2df8-455b-b661-57618e9b9f32"){
+        // Pretend that it gets the name from the DB instead of being hardcoded
+        if(bearerToken === mockBearerTokenWeExpect){
             return "George Smith";
         } else {
             throw new Error("Could not find an employee for that bearer token");
--- lessons_answers/ANSWER_8-better-contracts-via-aggregate-types.ts
+++ lessons_answers/ANSWER_8-better-contracts-via-aggregate-types.ts
@@ -1,5 +1,7 @@
 (()=>{  // <-- we use an Immediately Invoked Function Expression (IIFE) so that the types and functions don't bleed into the other lessons
 
+    const mockBearerTokenWeExpect = "785a31de-2df8-455b-b661-57618e9b9f32";
+
     interface IPaidTimeOff {
         reason: string,
         employeeId: string,
@@ -12,6 +14,8 @@
         verifiedDate: Date,
     }
 
+    // So here's the cool thing: We're going to reuse the original type, but then we'll utilize the aggregation operator to add new properties
+    //      ...But, more importantly, we will provide two functions so that it's clear which ones will stamp/verify the paid time off
     type IPaidTimeOffVerified = IPaidTimeOff & IVerifiedByEmployee;
 
     function PostAnAbsence(pto: IPaidTimeOff, bearerToken: string) : IPaidTimeOff {
@@ -19,7 +23,7 @@
         return pto;
     }
 
-    function PostAnVerifiedAbsence(pto: IPaidTimeOff, bearerToken: string) : IPaidTimeOffVerified {
+    function PostAVerifiedAbsence(pto: IPaidTimeOff, bearerToken: string) : IPaidTimeOffVerified {
         var verifierInfo = GetVerifierFromBearerToken(bearerToken);
         if(verifierInfo == undefined){
             throw new Error("This user is not authorized to verify");
@@ -31,11 +35,20 @@
         return verifiedPto;
     }
 
+    // Let's test the output of the newly improved version
+    var verifiedPto = PostAVerifiedAbsence({
+        reason: "I was sick",
+        employeeId: "1234567",
+        startDate: new Date(),
+        endDate: new Date("9999-12-31T00:00.000Z")
+    }, mockBearerTokenWeExpect)
+    console.log("This PTO was verified by " + verifiedPto.verifierEmployeeId + " on " + verifiedPto.verifiedDate); // No longer prints undefined
+
     function GetVerifierFromBearerToken(bearerToken: string) : IVerifiedByEmployee | undefined {
         // In the real world, this would be hitting an external service to look up the "claims" to check
         //      does this user have a claim that states that they can verify?
         //      if so, let's return who they are and stamp the time they opted to verify
-        if(bearerToken === "785a31de-2df8-455b-b661-57618e9b9f32"){
+        if(bearerToken === mockBearerTokenWeExpect){
             return {
                 verifierEmployeeId: "George Smith",
                 verifiedDate: new Date(),
